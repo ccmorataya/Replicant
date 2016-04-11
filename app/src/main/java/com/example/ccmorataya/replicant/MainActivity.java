fa48bdf9b6d438fca1f5e4071abb4d752c610559
Cristian Morataya <cris.morataya@gmail.com>
2016-04-10 21:30:50 -0600

    Permite la interacción entre editText, texView, button utilizando onClickListener

--- app/src/main/java/com/example/ccmorataya/replicant/MainActivity.java
+++ app/src/main/java/com/example/ccmorataya/replicant/MainActivity.java
@@ -2,12 +2,39 @@ package com.example.ccmorataya.replicant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    EditText editText;
+    Button button;
+    TextView textView;
+    String texto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.username);
+        button = (Button) findViewById(R.id.send);
+        textView = (TextView) findViewById(R.id.show);
+
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.send:
+                texto = editText.getText().toString();
+                textView.setText(texto);
+                break;
+            default:
+                break;
+        }
     }
 }
