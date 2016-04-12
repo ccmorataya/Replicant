e5e1509e3a1910726a119bbde659bd3e3b4be133
Cristian Morataya <cris.morataya@gmail.com>
2016-04-11 21:37:22 -0600

    Cambia el codigo, utilizacion de checkbox y button

--- app/src/main/java/com/example/ccmorataya/replicant/MainActivity.java
+++ app/src/main/java/com/example/ccmorataya/replicant/MainActivity.java
@@ -4,23 +4,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    EditText editText;
+//    EditText editText;
     Button button;
     TextView textView;
-    String texto;
+    CheckBox ch1, ch2;
+//    String texto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText = (EditText) findViewById(R.id.username);
+//        editText = (EditText) findViewById(R.id.username);
         button = (Button) findViewById(R.id.send);
         textView = (TextView) findViewById(R.id.show);
+
+        ch1 = (CheckBox) findViewById(R.id.chk1);
+        ch2 = (CheckBox) findViewById(R.id.chk2);
 
         button.setOnClickListener(this);
     }
@@ -30,8 +35,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId())
         {
             case R.id.send:
-                texto = editText.getText().toString();
-                textView.setText(texto);
+                    if (ch1.isChecked() == true)
+                    {
+                        textView.setText("Buenos días Señor");
+                    }
+                    else if (ch2.isChecked() == true)
+                    {
+                        textView.setText("Buenos dias Señora");
+                    }
                 break;
             default:
                 break;
