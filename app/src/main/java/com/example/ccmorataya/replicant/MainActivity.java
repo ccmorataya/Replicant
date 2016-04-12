5210de9c778ea027e306113226498e955ba9dbe1
Cristian Morataya <cris.morataya@gmail.com>
2016-04-11 22:15:46 -0600

    Implementa spinner e interaccion con el textView

--- app/src/main/java/com/example/ccmorataya/replicant/MainActivity.java
+++ app/src/main/java/com/example/ccmorataya/replicant/MainActivity.java
@@ -3,49 +3,38 @@ package com.example.ccmorataya.replicant;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-//    EditText editText;
-    Button button;
+public class MainActivity extends AppCompatActivity {
+    Spinner spinner;
     TextView textView;
-    CheckBox ch1, ch2;
-//    String texto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        editText = (EditText) findViewById(R.id.username);
-        button = (Button) findViewById(R.id.send);
-        textView = (TextView) findViewById(R.id.show);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        textView = (TextView) findViewById(R.id.textView);
 
-        ch1 = (CheckBox) findViewById(R.id.chk1);
-        ch2 = (CheckBox) findViewById(R.id.chk2);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.meses, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
-        button.setOnClickListener(this);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                textView.setText(parent.getItemAtPosition(position).toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId())
-        {
-            case R.id.send:
-                    if (ch1.isChecked() == true)
-                    {
-                        textView.setText("Buenos días Señor");
-                    }
-                    else if (ch2.isChecked() == true)
-                    {
-                        textView.setText("Buenos dias Señora");
-                    }
-                break;
-            default:
-                break;
-        }
-    }
 }
